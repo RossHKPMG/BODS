6d2b068a3cfa9fa0982d3d87df282b909cfbb473
RossHKPMG <ross.hamilton@kpmg.ie>
2023-08-14 16:53:47 +0100

    Merging GitHub repositories together and also adding in new data for Ross Travel

--- Python_Scripts/api_test.py
+++ Python_Scripts/api_test.py
@@ -8,9 +8,10 @@ data = requests.get(
     "https://data.bus-data.dft.gov.uk/api/v1/fares/dataset?noc=ROSS&status=published&limit=1&offset=0&api_key=ab70c6d46880d6fb3418a656e5c8e40fb5aa8f2c"
 )
 response_data = data.json()
+print(response_data)
 
-id = response_data["results"][0]["id"]
-zip = str(id) + ".zip"
+name_id = response_data["results"][0]["id"]
+data_download_folder = f"{str(name_id)}.zip"
 operator_name = response_data["results"][0]["operatorName"]
 url = response_data["results"][0]["url"]
 
