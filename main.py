0da5cc9bf57e6aa7d4f1bcce48721171868eec4b
RossHKPMG <ross.hamilton@kpmg.ie>
2023-08-14 12:59:20 +0100

    intial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import shutil
+import os
+from urllib.request import urlopen
+from shutil import copyfileobj
+
+data = requests.get(
+    "https://data.bus-data.dft.gov.uk/api/v1/fares/dataset?noc=ROSS&status=published&limit=1&offset=0&api_key=ab70c6d46880d6fb3418a656e5c8e40fb5aa8f2c"
+)
+response_data = data.json()
+
+id = response_data["results"][0]["id"]
+zip = str(id) + ".zip"
+operator_name = response_data["results"][0]["operatorName"]
+url = response_data["results"][0]["url"]
+
+parent_dir = "C:/Users/Ross/Documents/BODS/"
+path = os.path.join(parent_dir, operator_name)
+os.mkdir(path)
+
+with urlopen(url) as in_stream, open(zip, "wb") as out_file:
+    copyfileobj(in_stream, out_file)
+
+filepath = os.path.join(parent_dir, zip)
+shutil.move(filepath, path)
