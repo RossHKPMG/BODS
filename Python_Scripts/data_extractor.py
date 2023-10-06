df2eeb1d3d492a467ed93bfc504ef31c85dbe2ba
RossHKPMG <ross.hamilton@kpmg.ie>
2023-10-06 16:38:56 +0100

    Added in new code to handle FF and Route data extraction

--- Python_Scripts/data_extractor.py
+++ Python_Scripts/data_extractor.py
@@ -200,11 +200,10 @@ class FareDataExtractor:
         self.fz_df_combined = pd.DataFrame()
         self.t_df_combined = pd.DataFrame()
         self.actual_t_df_combined = pd.DataFrame()
+        self.ff_df_combined = pd.DataFrame()
 
 
     def get_fare_data(self):
-
-        error_list = []
 
         for operator in os.listdir(self.xml_folder):
             op_path = self.xml_folder + "/" + operator
@@ -222,7 +221,10 @@ class FareDataExtractor:
                         
                         self.p_type = self.get_p_type()
                         self.r_type = self.get_r_type()
-                        self.ff = self.get_ff()
+                        self.ff_amount = self.get_ff_amount()
+
+                        self.ff_df = self.create_flat_fare_df(self.p_type, self.r_type, self.ff_amount)
+                        self.ff_df_combined = pd.concat([self.ff_df_combined, self.ff_df], ignore_index=True, axis=0)
 
                     else:
         
@@ -253,28 +255,61 @@ class FareDataExtractor:
                         self.t_df_combined = pd.concat([self.t_df_combined, self.t_df], ignore_index=True, axis=0)
                         
                         print("DataFramesCombined")
-                
-                
-                x_list = os.listdir(noc_path)
-                if "_FF_" in x_list[0]:
-                    path = "C:/Users/rosshamilton/OneDrive - KPMG/Documents/BODS Data/Extracted Fare Data/" + operator + "/" + noc + "/"
-                    os.makedirs(path)
-                    break
-                
-                else:
+            
+
+                if len(self.ff_df_combined) > 1 and len(self.fz_df_combined) > 1:
+
+                    self.actual_t_df_combined = self.t_df_combined.drop(self.t_df_combined[self.t_df_combined['Route Cost (£)'] == '0.25'].index)
+
+                    self.fz_df_combined = self.fz_df_combined.drop_duplicates(subset=self.fz_df_combined.columns.difference(["Fare Zone Stop References"]))
+                    self.t_df_combined = self.t_df_combined.drop_duplicates()
+                    self.actual_t_df_combined = self.actual_t_df_combined.drop_duplicates()
+                    self.ff_df_combined = self.ff_df_combined.drop_duplicates()
+
+                    # Need to add in FF json conversion and dropping duplicates
+                    self.routes_to_json(self.fz_df_combined, self.t_df_combined, self.actual_t_df_combined, operator, noc)
+                    self.ff_to_json(self.ff_df_combined, operator, noc)
+
+                    self.fz_df_combined = pd.DataFrame()
+                    self.t_df_combined = pd.DataFrame()
+                    self.actual_t_df_combined = pd.DataFrame()
+                    self.ff_df_combined = pd.DataFrame()
+
+                elif len(self.ff_df_combined) == 0 and len(self.fz_df_combined) > 1:
+
                     self.actual_t_df_combined = self.t_df_combined.drop(self.t_df_combined[self.t_df_combined['Route Cost (£)'] == '0.25'].index)
 
                     self.fz_df_combined = self.fz_df_combined.drop_duplicates(subset=self.fz_df_combined.columns.difference(["Fare Zone Stop References"]))
                     self.t_df_combined = self.t_df_combined.drop_duplicates()
                     self.actual_t_df_combined = self.actual_t_df_combined.drop_duplicates()
 
-                    self.df_to_json(self.fz_df_combined, self.t_df_combined, self.actual_t_df_combined, operator, noc)
+                    # Need to add in FF json conversion and dropping duplicates
+                    self.routes_to_json(self.fz_df_combined, self.t_df_combined, self.actual_t_df_combined, operator, noc)
 
                     self.fz_df_combined = pd.DataFrame()
                     self.t_df_combined = pd.DataFrame()
                     self.actual_t_df_combined = pd.DataFrame()
+                    self.ff_df_combined = pd.DataFrame()
+                
+                elif len(self.ff_df_combined) > 1 and len(self.fz_df_combined) == 0:
 
-                break    
+                    self.ff_df_combined = self.ff_df_combined.drop_duplicates()
+                    self.ff_to_json(self.ff_df_combined, operator, noc)
+                    self.fz_df_combined = pd.DataFrame()
+                    self.t_df_combined = pd.DataFrame()
+                    self.actual_t_df_combined = pd.DataFrame()
+                    self.ff_df_combined = pd.DataFrame()
+                    
+
+                   
+
+
+
+
+
+
+
+
 
 
     ############################################################
@@ -293,20 +328,21 @@ class FareDataExtractor:
 
     def get_r_type(self):
         data = self.root.find("./Description", self.namespace)
-        if "outbound" in data.text.lower():
-            return "Outbound"
-        elif "inbound" in data.text.lower():
-            return "Inbound"
+        if data == None:
+            return "I/O"
         else:
-            return data.text.lower()
+            if "outbound" in data.text.lower():
+                return "Outbound"
+            elif "inbound" in data.text.lower():
+                return "Inbound"
+            else:
+                return "One Way"
         
-    def get_ff(self):
-        data = self.root.find("dataObjects/.//amount", self.namespace)
-        print(data.text)
+    def get_ff_amount(self):
+        data = self.root.find("dataObjects/.//TimeIntervalPrice/Amount", self.namespace)
+        print(data)
         return data.text
         
-
- 
     def get_fz_ids(self):
         data = self.root.findall("dataObjects/.//FareZone", self.namespace)
         x = [elt.attrib["id"].replace("fs@", "") for elt in data]
@@ -326,33 +362,6 @@ class FareDataExtractor:
                 y.append(["0000000000"])
         return y
     
-#     for l in range(len(i)):
-#    i[l] = i[l].attrib["ref"].replace("atco:", "")
-    
-    # def get_fz_stops(self):
-    #     data = self.root.findall("dataObjects/.//FareZone", self.namespace)
-    #     y=[]
-    #     x = [elt[:] for elt in data]
-    #     for i in data: 
-    #         if "membersGroups" in i[1].tag:
-    #             for l in range(len(i)):
-    #                 i[l] = i[l].attrib["ref"].replace("atco:", "")
-        
-    #     # for i in x:
-    #     #         for l in range(len(i)):
-    #     #             i[l] = i[l].attrib["ref"].replace("atco:", "")
-
-
-        return data
-    
-
-
-
-
-
-
-
-
     def get_fz_travelled(self):
         data = self.root.findall("dataObjects/.//distanceMatrixElements/", self.namespace)
         x = [elt.attrib["id"] for elt in data]
@@ -368,9 +377,6 @@ class FareDataExtractor:
         x = [elt.attrib["ref"].replace("fs@", "") for elt in data]
         return x
     
-
-
-
     def get_fz_price(self):
         data = self.root.findall("dataObjects/.//distanceMatrixElements/", self.namespace)
         x = [elt for elt in data]
@@ -401,6 +407,18 @@ class FareDataExtractor:
 
     ############################################################
     
+    def create_flat_fare_df(self, p_type, r_type, ff_amount):
+
+        ff_dict = {
+            "Passenger Type": p_type,
+            "Inbound/Outbound": r_type,
+            "Flat Fare Amount": ff_amount
+        }
+        ff_df = pd.DataFrame(ff_dict, index=[0])
+
+        return ff_df
+
+
     def create_fare_zones_df(self, fz_ids, fz_s_points, line_pid, r_type):
 
         fz_dict = {
@@ -441,339 +459,31 @@ class FareDataExtractor:
         t_df = t_df.drop(['Tariff Price Band'], axis=1)
         return t_df
     
-    def df_to_json(self, fz_df_combined, t_df_combined, actual_t_df_combined, operator, noc):
+    def routes_to_json(self, fz_df_combined, t_df_combined, actual_t_df_combined, operator, noc):
 
         path = "C:/Users/rosshamilton/OneDrive - KPMG/Documents/BODS Data/Extracted Fare Data/" + operator + "/" + noc + "/"
-        os.makedirs(path)
-        fz_df_combined.to_json(path + 'Fare_Zone_Data.json', orient='records')
-        t_df_combined.to_json(path + 'Ideal_Tariff_Data.json', orient='records')
-        actual_t_df_combined.to_json(path + 'Actual_Tariff_Data.json', orient='records')
+
+        if os.path.exists(path):
+            fz_df_combined.to_json(path + 'Fare_Zone_Data.json', orient='records')
+            t_df_combined.to_json(path + 'Ideal_Tariff_Data.json', orient='records')
+            actual_t_df_combined.to_json(path + 'Actual_Tariff_Data.json', orient='records')
+        else:
+            os.makedirs(path)
+            fz_df_combined.to_json(path + 'Fare_Zone_Data.json', orient='records')
+            t_df_combined.to_json(path + 'Ideal_Tariff_Data.json', orient='records')
+            actual_t_df_combined.to_json(path + 'Actual_Tariff_Data.json', orient='records')
 
         print(["Data Tables Created"])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# self.stop_ids = self.get_stop_ids()
-# print(self.stop_ids)
-# print(len(self.stop_ids))
-# self.stop_names = self.get_stop_names()
-# print(self.stop_names)
-# print(len(self.stop_names))
-
-
-
-
-# self.op_name = self.get_op_name()
-# print(self.op_name)
-# self.op_noc = self.get_op_noc()
-# print(self.op_noc)
-# self.line_id = self.get_line_id()
-# print(self.line_id)
-# self.line_name = self.get_line_name()
-# print(self.line_name)
-# self.line_type = self.get_line_type()
-# print(self.line_type)
-# self.fz_type = self.get_fz_type()
-# print(self.fz_type)
-
-
-
-
-# def create_bus_stops_df(self, stop_ids, stop_names):
-
-#     s_dict = {
-#         "Stop ID": stop_ids,
-#         "Stop Name": stop_names
-#     }   
-#     s_df = pd.DataFrame(s_dict)
-#     return s_df
-
-# def create_route_df(self, line_id, line_pid, p_type, r_type):
-
-#     r_dict = {
-#         "Route ID": line_id,
-#         "Route Public ID": line_pid,
-#         "Passenger Type": p_type,
-#         "Inbound/Outbound": r_type,
-#         "Ticket Type": "Single"
-#     }
-#     r_df = pd.DataFrame(r_dict, index=[0])
-#     return r_df
-
-
-
-# def get_op_name(self):
-#     data = self.root.find("dataObjects/.//ResourceFrame/organisations/Operator/Name", self.namespace)
-#     return data.text
-
-# def get_op_noc(self):
-#     data = self.root.find("dataObjects/.//ResourceFrame/organisations/Operator/PublicCode", self.namespace)
-#     return data.text
-
-# def get_line_id(self):
-#     data = self.root.find("dataObjects/.//ServiceFrame/lines/Line", self.namespace)
-#     return data.attrib["id"]
-
-# def get_line_name(self):
-#     data = self.root.find("dataObjects/.//ServiceFrame/lines/Line/Name", self.namespace)
-#     return data.text
-
-# def get_line_type(self):
-#     data = self.root.find("dataObjects/.//ServiceFrame/lines/Line/LineType", self.namespace)
-#     return data.text
-
-# def get_fz_type(self):
-#     data = self.root.find("dataObjects/.//FareFrame//Tariff/TariffBasis", self.namespace)
-#     return data.text
-
-
-# def get_stop_ids(self):
-#     data = self.root.findall("dataObjects/.//ScheduledStopPoint", self.namespace)
-#     x = [elt.attrib["id"].replace("atco:", "") for elt in data]
-#     return x
-
-# def get_stop_names(self):
-#     data = self.root.findall("dataObjects/.//ScheduledStopPoint/Name", self.namespace)
-#     x = [elt.text for elt in data]
-#     return x
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ### OLD CODE ###
-
-    
-    # def create_m_df(self, op_name, op_noc, line_id, line_name, line_type, line_pid, fz_type, p_type, r_type):
-    #     dict = {
-    #         "Operator Name": op_name,
-    #         "Operator NOC": op_noc,
-    #         "Line ID": line_id,
-    #         "Line Public ID": line_pid,
-    #         "Line Name": line_name,
-    #         "Line Type": line_type,
-    #         "Fare Zone Type": fz_type,
-    #         "Passenger Type": p_type,
-    #         "Ticket Type": "Single",
-    #         "Route Type": r_type
-    #     }
-    #     df = pd.DataFrame([dict])
-    #     return df
-    
-    # def create_r_df(self, stop_ids, stop_names):
-
-    #     dict = {"Stop ID": stop_ids, "Stop Name": stop_names}   
-    #     df = pd.DataFrame(dict)
-    #     return df
-    
-    # def create_fz_df(self, fz_ids, fz_names, fz_s_points):
-
-    #     dict = {
-    #         "Fare Zone ID": fz_ids,
-    #         "Fare Zone Names": fz_names,
-    #         "Fare Zone Stop Reference": fz_s_points,
-    #     }
-    #     df = pd.DataFrame(dict)
-    #     df = df.explode("Fare Zone Stop Reference")
-
-    #     temp_df = pd.DataFrame()
-    #     pairings = list(itertools.combinations(df["Fare Zone Stop Reference"], r=2))
-    #     p1 = []
-    #     p2 = []
-    #     for pairs in pairings:
-    #         p1.append(pairs[0])
-    #         p2.append(pairs[1])
-    #     temp_df["Fare Zone Stop Reference (Start)"] = p1
-    #     temp_df["Fare Zone Stop Reference (End)"] = p2
-
-    #     fz_start_df = df.merge(
-    #         temp_df,
-    #         left_on="Fare Zone Stop Reference",
-    #         right_on="Fare Zone Stop Reference (Start)",
-    #     )
-    #     cols = ["Fare Zone ID", "Fare Zone Names", "Fare Zone Stop Reference (Start)"]
-    #     fz_start_df = fz_start_df[cols]
-
-    #     fz_end_df = pd.merge(
-    #         df,
-    #         temp_df,
-    #         left_on="Fare Zone Stop Reference",
-    #         right_on="Fare Zone Stop Reference (End)",
-    #         how="right",
-    #     )
-    #     cols = ["Fare Zone ID", "Fare Zone Names", "Fare Zone Stop Reference (End)"]
-    #     fz_end_df = fz_end_df[cols]
-
-    #     fz_end_df.rename(
-    #         {"Fare Zone ID": "Fare Zone ID E", "Fare Zone Names": "Fare Zone E"},
-    #         axis=1,
-    #         inplace=True,
-    #     )
-
-    #     fz_df = pd.concat([fz_start_df, fz_end_df], axis=1, join="inner")
-    #     return fz_df
-    
-    # def create_t_df(self, fz_travelled, fz_travelled_start, fz_travelled_end, fz_travelled_price):
-
-    #     dict = {
-    #         "Tariff ID": fz_travelled,
-    #         "Tariff Start Zone": fz_travelled_start,
-    #         "Tariff End Zone": fz_travelled_end,
-    #         "Tariff Price Band": fz_travelled_price,
-    #     }
-
-    #     df = pd.DataFrame(dict)
-
-    #     return df
-    
-    # def df_combination(self, temp_df1, temp_df2, temp_df3):
-
-    #     route_stops_df_start = temp_df2.merge(temp_df1, left_on='Fare Zone Stop Reference (Start)', right_on='Stop ID', how='left')
-    #     route_stops_df_start = route_stops_df_start.drop(['Stop ID'], axis=1)
-    #     cols = ['Fare Zone ID', 'Fare Zone Names','Fare Zone Stop Reference (Start)', 'Stop Name']
-    #     route_stops_df_start = route_stops_df_start[cols]
-
-        
-
-    #     route_stops_df_end = temp_df2.merge(temp_df1, left_on='Fare Zone Stop Reference (End)', right_on='Stop ID', how = 'left')
-    #     route_stops_df_end = route_stops_df_end.drop(['Stop ID'], axis=1)
-    #     route_stops_df_end.rename({'Stop Name': 'Stop Name E'},axis=1, inplace=True)
-    #     cols = ['Fare Zone ID E', 'Fare Zone E', 'Fare Zone Stop Reference (End)', 'Stop Name E']
-    #     route_stops_df_end = route_stops_df_end[cols]
-
-        
-
-    #     route_stops_df = pd.concat([route_stops_df_start, route_stops_df_end], axis=1, join='inner')
-
-    #     combined_df = pd.merge(route_stops_df, temp_df3,  how='left', left_on=['Fare Zone ID','Fare Zone ID E'], right_on = ['Tariff Start Zone','Tariff End Zone'])
-
-    #     for i in range(len(combined_df)):
-    #         x = re.sub(r'[^\d]+', '', combined_df.loc[i, 'Fare Zone ID'])
-    #         y = re.sub(r'[^\d]+', '', combined_df.loc[i, 'Fare Zone ID E'])
-
-    #         if pd.isna(combined_df.loc[i, 'Tariff ID']):
-    #             combined_df.loc[i, 'Tariff ID'] = x + '+' + y
-    #             combined_df.loc[i, 'Tariff Price Band'] = 'price_band_0.25'
-
-    #     cols = ['Tariff ID', 'Fare Zone ID', 'Fare Zone Names', 'Fare Zone Stop Reference (Start)', 'Stop Name', 'Fare Zone ID E', 'Fare Zone E', 'Fare Zone Stop Reference (End)', 'Stop Name E', 'Tariff Price Band']
-        
-    #     money = []
-    #     for i in combined_df['Tariff Price Band']:
-
-    #         x = re.sub('\D', '', i)
-    #         x = float(x)
-    #         money.append(x)
-
-    #     combined_df['Route Cost (£)'] = money
-
-    #     combined_df = combined_df.drop(['Tariff Price Band'], axis=1)
-
-    #     combined_df['Fare Zone Stop Reference (Start)'] = combined_df['Fare Zone Stop Reference (Start)'].replace('atco:', '', regex=True)
-    #     combined_df['Fare Zone Stop Reference (End)'] = combined_df['Fare Zone Stop Reference (End)'].replace('atco:', '', regex=True)
-
-    #     combined_df.rename({'ID': 'Line ID',
-    #                                 'Name': 'Line Name',
-    #                                 'Public Code':'Line Public Code',
-    #                                 'Tariff ID': 'Tariff Route ID',
-    #                                 'Fare Zone ID': 'Fare Zone Start ID',
-    #                                 'Fare Zone Names': 'Fare Zone Start Name',
-    #                                 'Fare Zone Stop Reference (Start)':'Tariff Route Start ID',
-    #                                 'Stop Name':'Tariff Route Start Name',
-    #                                 'Fare Zone ID E': 'Fare Zone End ID',
-    #                                 'Fare Zone E': 'Fare Zone End Name',
-    #                                 'Fare Zone Stop Reference (End)':'Tariff Route End ID',
-    #                                 'Stop Name E':'Tariff Route End Name',
-    #                                 'Route Cost (£)':'Tariff Route Cost (£)'},axis=1, inplace=True)
-        
-    #     cols = ['Tariff Route ID', 'Fare Zone Start ID', 'Fare Zone Start Name', 'Tariff Route Start Name', 'Tariff Route Start ID', 'Fare Zone End ID', 'Fare Zone End Name', 'Tariff Route End Name', 'Tariff Route End ID', 'Tariff Route Cost (£)']
-    #     combined_df = combined_df[cols]
-
-    #     combined_df['Operator Name'] = self.op_name
-    #     combined_df['Operator NOC'] = self.op_noc
-    #     combined_df['Line ID'] = self.line_id
-    #     combined_df['Line Public ID'] = self.line_pid
-    #     combined_df['Line Name'] = self.line_name
-    #     combined_df['Line Type'] = self.line_type
-    #     combined_df['Fare Zone Type'] = self.fz_type
-    #     combined_df['Passenger Type'] = self.p_type
-    #     combined_df['Ticket Type'] = "Single"
-    #     combined_df['Route'] = self.r_type
-        
-    #     return combined_df
-    
-
-
-
-
-
-
-                
+    def ff_to_json(self, ff_df_combined, operator, noc):
+
+        path = "C:/Users/rosshamilton/OneDrive - KPMG/Documents/BODS Data/Extracted Fare Data/" + operator + "/" + noc + "/"
+        if os.path.exists(path):
+            ff_df_combined.to_json(path + 'Flat_Fare_Data.json', orient='records')
+        else:
+            os.makedirs(path)
+            ff_df_combined.to_json(path + 'Flat_Fare_Data.json', orient='records')
+
+        print(["Data Tables Created"])
+     
             
